c949af9ed3de8b284ba8ba85d6493b8a6a0c8029
Johnny Code <epluscode@gmail.com>
2022-02-07 23:15:36 +0300

    select partially

---
+++ custom.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const element = document.querySelector('.js-choice');
+  const choices = new Choices(element, {
+  searchEnabled: false,
+  });
+});
+
