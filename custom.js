a978a8a78669b8e0be5936b6536f6344fd3d6fb6
pfellow <pfellow@gmail.com>
2022-02-08 01:03:22 +0300

    custom select: solved problem with is__flipped in Edge

--- custom.js
+++ custom.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', function(){
   const element = document.querySelector('.js-choice');
   const choices = new Choices(element, {
   searchEnabled: false,
+  classNames: {
+    flippedState: false,
+  }
   });
 });
 
