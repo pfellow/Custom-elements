88385c8111306d765468249ca679e112029eb55b
pfellow <pfellow@gmail.com>
2022-02-08 01:08:37 +0300

    custom select: removed sorting

--- custom.js
+++ custom.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
   const element = document.querySelector('.js-choice');
   const choices = new Choices(element, {
   searchEnabled: false,
+  shouldSort: false,
   classNames: {
     flippedState: false,
   }
