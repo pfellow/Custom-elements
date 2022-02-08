2b506ea4cc49d7242b48a83fe3e69d91e57ac304
Johnny Code <epluscode@gmail.com>
2022-02-08 21:45:30 +0300

    yandex map added

--- custom.js
+++ custom.js
@@ -7,5 +7,30 @@ document.addEventListener('DOMContentLoaded', function(){
     flippedState: false,
   }
   });
+
+    // Функция ymaps.ready() будет вызвана, когда
+    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+    ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [48.856663, 2.351556],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 12,
+            controls: []
+        });
+        var myPlacemark = new ymaps.Placemark([48.872185, 2.354224], {}, {
+          iconLayout: 'default#image',
+          iconImageHref: 'tag.svg',
+          iconImageSize: [42, 60],
+          iconImageOffset: [-21, -60]
+      });
+        myMap.geoObjects.add(myPlacemark);
+    }
 });
 
